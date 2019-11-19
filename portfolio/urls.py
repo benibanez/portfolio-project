084ad3124ece625a0299a7bcf5d26b7c217f6dbd
benibanez <50468874+benibanez@users.noreply.github.com>
2019-11-18 21:46:38 -0500

    Blog functionality and polish

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     url('^admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
